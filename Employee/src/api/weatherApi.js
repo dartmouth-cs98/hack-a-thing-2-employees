dcfee0f501af30a4e490e65e53beddba846a00ca
Weijia Tang <weijia.tang.18@dartmouth.edu>
2017-09-25 18:36:02 -0400

    minor

--- Employee/src/api/weatherApi.js
+++ Employee/src/api/weatherApi.js
@@ -15,17 +15,17 @@ class WeatherApi {
     static getAllWeatherNodes() {
         return new Promise((resolve, reject) => setTimeout(() => resolve(weatherList)), 1200);
     }
-    
+
     static searchByLocation(qs) {
         return new Promise((resolve, reject) => {
             let result = weatherList.filter(employee => {
                 const fullName = `${weatherList.city.toLowerCase()} ${weatherList.state.toLowerCase()}`;
                 return fullName.indexOf(qs.toLowerCase()) > -1;
             });
-            
+
             resolve(result);
         })
     }
 }
 
-export default weatherApi;
+export default WeatherApi;
